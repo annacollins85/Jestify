bcea0757e3f51bfec1c29fe248ca984b1869c529
Anna Collins <annacollins85@gmail.com>
2018-04-05 10:44:47 +0200

    before change to vue.extend

--- tests/unit/Checkbox.spec.js
+++ tests/unit/Checkbox.spec.js
@@ -1,7 +1,10 @@
-import { shallow } from '@vue/test-utils'
-import { createRenderer } from 'vue-server-renderer'
-
+import Vue from 'vue'
 import Checkbox from '@/components/Checkbox/Checkbox.vue'
+
+import { shallow } from '@vue/test-utils'
+const jsdom = require('jsdom')
+
+const renderer = require('vue-server-renderer').createRenderer()
 
 describe('Checkbox.vue', () => {
   it('renders the name when passed', () => {
@@ -9,7 +12,14 @@ describe('Checkbox.vue', () => {
     const wrapper = shallow(Checkbox, {
       propsData: { name }
     })
-    expect(wrapper.text()).toMatch(name)
+    renderer.renderToString(wrapper, (err, str) => {
+      if (err) console.log('error')
+      const dom = new jsdom.JSDOM(str)
+      console.log(dom.window.document)
+      const name = dom.window.document.querySelector('h4')
+      expect(name.textContent).toContain('name of Checkbox')
+    })
+    // expect(wrapper.text()).toMatch(name)
   })
   it('renders the description when passed', () => {
     const description = 'description of Checkbox'
@@ -18,23 +28,23 @@ describe('Checkbox.vue', () => {
     })
     expect(wrapper.text()).toMatch(description)
   })
-  it('should toggle checked status when clicked', () => {
-    const wrapper = shallow(Checkbox)
-    wrapper.trigger('click')
-    expect(wrapper.props().checked).toBe(true)
-    wrapper.trigger('click')
-    expect(wrapper.props().checked).toBe(true)
-  })
-  it('matches snapshot', () => {
-    const name = 'Checkbox name'
-    const description = 'Checkbox description'
-    const renderer = createRenderer()
-    const wrapper = shallow(Checkbox, {
-      propsData: { name, description }
-    })
-    renderer.renderToString(wrapper.vm, (err, str) => {
-      if (err) throw new Error(err)
-      expect(str).toMatchSnapshot()
-    })
-  })
+  // it('should toggle checked status when clicked', () => {
+  //   const wrapper = shallow(Checkbox)
+  //   wrapper.trigger('click')
+  //   expect(wrapper.props().checked).toBe(true)
+  //   wrapper.trigger('click')
+  //   expect(wrapper.props().checked).toBe(true)
+  // })
+  // it('matches snapshot', () => {
+  //   const name = 'Checkbox name'
+  //   const description = 'Checkbox description'
+  //   const renderer = createRenderer()
+  //   const wrapper = shallow(Checkbox, {
+  //     propsData: { name, description }
+  //   })
+  //   renderer.renderToString(wrapper.vm, (err, str) => {
+  //     if (err) throw new Error(err)
+  //     expect(str).toMatchSnapshot()
+  //   })
+  // })
 })
--- tests/unit/Input.spec.js
+++ tests/unit/Input.spec.js
@@ -1,5 +1,5 @@
 import { shallow } from '@vue/test-utils'
-import { createRenderer } from 'vue-server-renderer'
+// import { createRenderer } from 'vue-server-renderer'
 
 import Input from '@/components/Input/Input.vue'
 
@@ -11,22 +11,24 @@ describe('Input.vue', () => {
     })
     expect(wrapper.text()).toMatch(label)
   })
-  it('has a max length when passed', () => {
-    const maxLength = 10
-    const wrapper = shallow(Input, {
-      propsData: { maxLength }
-    })
-    expect(wrapper.props().maxLength).toBe(10)
-  })
-  it('matches snapshot', () => {
-    const label = 'Input label'
-    const renderer = createRenderer()
-    const wrapper = shallow(Input, {
-      propsData: { label }
-    })
-    renderer.renderToString(wrapper.vm, (err, str) => {
-      if (err) throw new Error(err)
-      expect(str).toMatchSnapshot()
-    })
-  })
+  // it('has a max length when passed', () => {
+  //   const maxLength = 10
+  //   const wrapper = shallow(Input, {
+  //     propsData: { maxLength }
+  //   })
+  //   // check tohtml has attribute maxLength
+  //   console.log((wrapper.find('form-control').attributes()))
+  //   expect(wrapper.props().maxLength).toBe(10)
+  // })
+  // it('matches snapshot', () => {
+  //   const label = 'Input label'
+  //   const renderer = createRenderer()
+  //   const wrapper = shallow(Input, {
+  //     propsData: { label }
+  //   })
+  //   renderer.renderToString(wrapper.vm, (err, str) => {
+  //     if (err) throw new Error(err)
+  //     expect(str).toMatchSnapshot()
+  //   })
+  // })
 })
